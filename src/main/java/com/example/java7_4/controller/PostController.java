43711fe1471cff607d5e614ea1fee70970935077
Tan Yawen <2749934612@qq.com>
2025-03-04 15:31:28 +0800

    /sendPostWithMutiImages接口实现(/sendPost接口的支持多张图版本)

--- src/main/java/com/example/java7_4/controller/PostController.java
+++ src/main/java/com/example/java7_4/controller/PostController.java
@@ -18,9 +18,7 @@ import org.springframework.web.servlet.view.RedirectView;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.util.*;
 
 @RestController
 @RequestMapping("/post")
@@ -89,6 +87,68 @@ public class PostController {
             return Result.error("上传图片失败");
         }
 
+
+        int cnt=postService.save(post);
+        if(cnt==1){
+            redisTemplate.delete(RedisKeyConstants.POSTS);//删除Redis里头的posts缓存
+            return Result.success(Boolean.TRUE);
+        }else{
+            return Result.error("发送帖子失败");
+        }
+
+    }
+
+
+    @PostMapping("/sendPostWithMutiImages")
+    public Result<Boolean> sendPostWithMutiImages(@RequestHeader("Authorization") String authorization
+            , @RequestParam("text") String text
+            , @RequestParam("images") List<MultipartFile> files) {
+
+        Long userId= BaseContext.getCurrentId();
+
+        Post post=new Post();
+        post.setUserId(userId);
+        post.setPostLike(0L);
+        post.setPostText(text);
+
+        //保存图片
+        // 获取项目根目录
+        String rootDirectory = System.getProperty("user.dir");
+
+        // 定义项目外部的上传路径
+        String uploadDirectory = rootDirectory + File.separator + "static" + File.separator + "images";
+        // 创建文件名，使用用户ID作为前缀
+
+
+
+
+
+
+        List<String> postImgPathList=new ArrayList<String>();
+        for(MultipartFile file:files){
+            try {
+                // 创建目录（如果不存在）
+                File directory = new File(uploadDirectory);
+                if (!directory.exists()) {
+                    directory.mkdirs();
+                }
+                String imgPath="/posts/post"+ UUID.randomUUID()+".jpg";
+                String filePath =  uploadDirectory+imgPath;
+                // 将文件保存到指定路径
+                File dest = new File(filePath);
+                file.transferTo(dest);
+
+                postImgPathList.add(imgPath);
+                log.info("logTag::sendPostWithMutiImages:filePath={}",filePath);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                return Result.error("上传图片失败");
+            }
+        }
+
+
+        post.setPostImgPath(String.join("@_@",postImgPathList));
 
         int cnt=postService.save(post);
         if(cnt==1){
